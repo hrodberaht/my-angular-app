773c2531118cc3571ee4dc28753d866515e2353b
Robert <seedraev@gmail.com>
2017-09-18 15:25:06 +0000

    add styles for clock and app components

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-clock',
-    templateUrl: './clock.component.html'
+    templateUrl: './clock.component.html',
+    styleUrls : ['./clock.component.css']
 })
 export class ClockComponent {
     time = '12:43';
