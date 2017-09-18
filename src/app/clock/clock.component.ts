b44d03fb143ef67a4235e5c5f8b60e0d2ce71bf2
Robert <seedraev@gmail.com>
2017-09-18 15:21:05 +0000

    add clock component

---
+++ src/app/clock/clock.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-clock',
+    templateUrl: './clock.component.html'
+})
+export class ClockComponent {
+    time = '12:43';
+}
