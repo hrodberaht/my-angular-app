eeb934e210a76df4c1513d87cfe3ed329ff1907b
Robert <seedraev@gmail.com>
2017-10-05 11:21:20 +0000

    clear unused commponet like server, servers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { ClockComponent } from './clock/clock.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './product/product.component';
@@ -13,8 +11,6 @@ import { RegistrationComponent} from './registration/registration.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
     ClockComponent,
     ProductsComponent,
     ProductComponent,
